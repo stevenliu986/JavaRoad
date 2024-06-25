dfb663bbb73c192a9e4fccf7066b89be36813f52
StevenLiu <stevenliu986@gmail.com>
2024-06-25 22:29:59 +1000

    add files

---
+++ chapter01_basic/src/com/javalearning/oop01/StudentTest.java
@@ -0,0 +1,10 @@
+package com.javalearning.oop01;
+
+public class StudentTest {
+    public static void main(String[] args) {
+        Student student = new Student();
+
+        student.setAge(23);
+        System.out.println(student.getAge());
+    }
+}
