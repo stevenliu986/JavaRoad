ecb63126b9b0152bd5b3d7fb3340895d4bce9767
StevenLiu <stevenliu986@gmail.com>
2024-09-18 21:22:54 +1000

    add test class

---
+++ chapter03_maven/src/test/java/com/javalearning/TestHello.java
@@ -0,0 +1,11 @@
+package com.javalearning;
+
+import org.junit.Test;
+
+public class TestHello {
+    @Test
+    public void test() {
+        Util util = new Util();
+        util.hello();
+    }
+}
